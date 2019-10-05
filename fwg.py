f26e5e31bfaf5953ca9ea81b62f3acfc237b555f
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-04 21:27:17 -0700

    Work in progress

---
+++ fwg.py
@@ -0,0 +1,20 @@
+'''
+
+    fire vs water
+    fire vs grass
+    water vs fire
+
+    water vs grass
+    grass vs fire
+    grass vs water
+
+    fire = rock
+    paper = water
+    grass = scissors
+'''
+
+print('Fight!\n')
+print('*' * 9)
+
+type = ['fire', 'water', 'grass']
+wins = ['']
