5bd230b9496e99be331a9605b28cedebfe37f369
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-07 19:51:45 -0700

    Messing with functions

--- fwg.py
+++ fwg.py
@@ -1,20 +1,32 @@
-'''
-
-    fire vs water
-    fire vs grass
-    water vs fire
-
-    water vs grass
-    grass vs fire
-    grass vs water
-
-    fire = rock
-    paper = water
-    grass = scissors
-'''
+import random
 
 print('Fight!\n')
 print('*' * 9)
 
 type = ['fire', 'water', 'grass']
-wins = ['']
+
+
+wins = ['firegrass', 'waterfire', 'grasswater']
+
+def game():
+    while True:
+        user_choice = input('Choose your type: [fire, water, grass] ').lower()
+        comp_choice = random.choice(type)
+        print(f'You chose: {user_choice} and I chose: {comp_choice}')
+        if user_choice not in type:
+            print('Please choose a valid option...')
+        elif user_choice == comp_choice:
+            print('Tie game')
+        elif comp_choice in wins:
+            print('You won!')
+        else:
+            print('You lost...')
+
+game()
+
+print('Would you like to play again? y/n')
+answer = input()
+if answer == 'y':
+    game()
+elif answer == 'n':
+    quit()
