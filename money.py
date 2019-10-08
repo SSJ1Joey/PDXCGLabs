95744b7a901e7a9bca681a9b14c5ff93c76c762f
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-07 20:27:24 -0700

    Coins only

--- money.py
+++ money.py
@@ -2,3 +2,13 @@ print('Lets make some change!')
 
 def main():
     amount = int(input('Please enter an amount in cents: '))
+    print(amount // 100, 'Dollars')
+    amount = amount % 100
+    print(amount // 25, 'Quarters')
+    amount = amount % 25
+    print(amount // 10, 'Dimes')
+    amount = amount % 10
+    print(amount // 5, 'Nickles')
+    amount = amount % 5
+    print(amount // 1, 'Pennies')
+    amount = amount % 1
