d2a33f5819c921780ad4adbfb04f0b9d26fa3776
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-07 21:12:50 -0700

    Version 2

--- unit.py
+++ unit.py
@@ -11,11 +11,21 @@ def mile():
     print(unit * 1609.3, ': Miles')
 
 def meter():
-    unit = int(input('How many Meters want to convert to Meters?: '))
+    unit = int(input('How many Meters do you want to convert to Meters?: '))
     print(f'You typed in {unit} Meters.')
     print(unit, ': Meters')
 
-def kilometers():
+def kilometer():
     unit = int(input('How many Kilometers do you want to convert to Meters?: '))
     print(f'You typed in {unit} Kilometers.')
     print(unit * 1000, ': Meters' )
+
+answer = input('What would you like to convert to Meters? (Feet, Miles, Meters or Kilometers)').lower()
+if answer == 'feet':
+    print(foot())
+elif answer == 'miles':
+    print(mile())
+elif answer == 'meters':
+    print(meters())
+elif answer == 'kilometers':
+    print(kilometer())
