128aedcf1bc198cd20582d413eabd5a112682d82
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-08 19:51:31 -0700

    Start

---
+++ the_calc.py
@@ -0,0 +1,22 @@
+
+ops = ['+', '-', '*', '/']
+
+def calc():
+    if user_op == '+':
+        user_num + user_num2
+    elif user_op == '-':
+        user_num - user_num2
+    elif user_op == '*':
+        user_num * user_num2
+    elif user_op == '/':
+        user_num / user_num2
+
+
+
+
+
+
+def main():
+    user_op = input("What is the operation you'd like to perform?: ")
+    user_num = int(input('What is your first number?: '))
+    user_num2 = int(input('What is your second number?: '))
