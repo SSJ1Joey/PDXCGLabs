2bf98710099b5a54d7b2606a3c4ee83496b3b92e
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-09 20:52:50 -0700

    Need to figure out loop

--- guess_the_num.py
+++ guess_the_num.py
@@ -1,8 +1,7 @@
 import random
 
-print("Welcome to guess the number! I'm sorry you're bored...")
+print("Welcome to guess the number! I'm sorry you're bored...\n")
 
-attempts = 0
 
 nums = ('1','2','3','4','5','6','7','8','9','10')
 
@@ -10,17 +9,18 @@ def game():
     while True:
         print('Try and guess what number im guessing... between 1 and 10: ' )
         user_choice = input()
+        attempts = 0
         comp_choice = random.choice(nums)
         if user_choice == comp_choice:
             print('You guessed correct!')
             break
         elif user_choice < comp_choice:
-            print("You're cold...")
-            attempts = attempts + 1 
+            print("You're too low...")
+            attempts += 1 
             print('Attempts:',attempts)
         else:
-            print("You're hot...")
-            attempts = attempts + 1
+            print("You're too high...")
+            attempts += 1
             print('Attempts:',attempts)
 
 game()
