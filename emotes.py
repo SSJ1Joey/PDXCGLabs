ab8745505e29ef99236f2271c8e62d4d27b749c1
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-11 11:27:13 -0700

    :tada:

--- emotes.py
+++ emotes.py
@@ -7,10 +7,11 @@ nose = ['o', '-', '0', 'V', '3']
 mouth = [')', 'D', ']', '(', 'o']
 
 while True:
-    print('Would you like to see a face? yes/no')
+    print('Would you like to see a face? y/n')
     answer = input()
-    if answer == 'yes':
-        print(f'Much better! {random.choice(eyes)}{random.choice(nose)}{random.choice(mouth)}')
+    if answer == 'y':
+        for item in range(5):
+            print(f'{random.choice(eyes)}{random.choice(nose)}{random.choice(mouth)}')
     else:
         print('Thanks for playing!')
         break
