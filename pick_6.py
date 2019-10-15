c9dd7ddedeb504ac49b83ca961e2d84449a508d2
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-14 19:30:39 -0700

    Meh

--- pick_6.py
+++ pick_6.py
@@ -6,50 +6,21 @@ balance = 0
 def pick_6():
     luck = range(1, 100)
     numbers = []
-    first = random.choice(luck) 
-    second = random.choice(luck) 
-    third = random.choice(luck) 
-    forth = random.choice(luck) 
-    fifth = random.choice(luck) 
-    sixth = random.choice(luck) 
-    numbers.append(first)
-    numbers.append(second)
-    numbers.append(third)
-    numbers.append(forth)
-    numbers.append(fifth)
-    numbers.append(sixth)
+    for i in range(6):
+        numbers.append(random.choice(luck))
+    print(numbers)
     
-    
-    
-
 def ticket():
-    pick_6()
-    t_list = []
-    for item in range(6):
-        nums = int(input('Please pick 6 numbers from 1 - 99: '))
-        t_list.append(nums)
-    print(f'You chose: {t_list}')
+    comp_ticket = pick_6()
+    user_ticket = pick_6()
 
-    if pick_6() == t_list:
-        balance += 25000000
-    elif pick_6() == t_list[1]:
-        balance += 100
-    elif pick_6() == t_list[2]:
-        balance += 100
+def game():
+    single_win = comp_ticket[1] == user_ticket[1]
 
+game()
     
     
     
-pick_6()  
-ticket()       
 
 
-
-
-
-
-
-
-
-
-
+  
