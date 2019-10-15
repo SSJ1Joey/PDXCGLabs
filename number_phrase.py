bd8bfb8b7ef39fab927758d87e5264da583e0adf
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-14 21:12:42 -0700

    Wrong way?

---
+++ number_phrase.py
@@ -0,0 +1,9 @@
+low_nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+
+x = int(input('Give me a number to spell out: '))
+
+abc = low_nums[x]
+
+print('Here is your number spelled out:',abc)
+
+
