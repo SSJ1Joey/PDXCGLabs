7741db7a183587f0f2ff06cc934d1b95255dd265
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-15 21:24:28 -0700

    :tada:

--- number_phrase.py
+++ number_phrase.py
@@ -4,28 +4,31 @@ tens = ['twenty-', 'thirty-', 'forty-', 'fifty-', 'sixty-', 'seventy-', 'eighty-
 
 
 def spell():
-    x = int(input('Give me a number to spell out: '))
-    
-    if x > 0 and x <= 10:
-        print('Here is your number spelled out:',low_nums[x])
-    elif x > 10 and x < 20:
-        print('Here is your number spelled out:',teens[x - 11])
-    elif x >= 20 and x < 30:
-        print(f'Here is your number spelled out: {tens[0]}{low_nums[x - 20]}')
-    elif x >= 30 and x < 40:
-        print(f'Here is your number spelled out: {tens[1]}{low_nums[x - 30]}')
-    elif x >= 40 and x < 50:
-        print(f'Here is your number spelled out: {tens[2]}{low_nums[x - 40]}')
-    elif x >= 50 and x < 60:
-        print(f'Here is your number spelled out: {tens[3]}{low_nums[x - 50]}')
-    elif x >= 60 and x < 70:
-        print(f'Here is your number spelled out: {tens[4]}{low_nums[x - 60]}')
-    elif x >= 70 and x < 80:
-        print(f'Here is your number spelled out: {tens[5]}{low_nums[x - 70]}')
-    elif x >= 80 and x < 90:
-        print(f'Here is your number spelled out: {tens[6]}{low_nums[x - 80]}')
-    elif x >= 90 and x < 100:
-        print(f'Here is your number spelled out: {tens[7]}{low_nums[x - 90]}')
+    while True:
+        x = int(input('Give me a number to spell out: '))
+        
+        if x > 0 and x <= 10:
+            print('Here is your number spelled out:',low_nums[x])
+        elif x > 10 and x < 20:
+            print('Here is your number spelled out:',teens[x - 11])
+        elif x >= 20 and x < 30:
+            print(f'Here is your number spelled out: {tens[0]}{low_nums[x - 20]}')
+        elif x >= 30 and x < 40:
+            print(f'Here is your number spelled out: {tens[1]}{low_nums[x - 30]}')
+        elif x >= 40 and x < 50:
+            print(f'Here is your number spelled out: {tens[2]}{low_nums[x - 40]}')
+        elif x >= 50 and x < 60:
+            print(f'Here is your number spelled out: {tens[3]}{low_nums[x - 50]}')
+        elif x >= 60 and x < 70:
+            print(f'Here is your number spelled out: {tens[4]}{low_nums[x - 60]}')
+        elif x >= 70 and x < 80:
+            print(f'Here is your number spelled out: {tens[5]}{low_nums[x - 70]}')
+        elif x >= 80 and x < 90:
+            print(f'Here is your number spelled out: {tens[6]}{low_nums[x - 80]}')
+        elif x >= 90 and x < 100:
+            print(f'Here is your number spelled out: {tens[7]}{low_nums[x - 90]}')
+        elif x == 0:
+            break
 
 spell()
 
