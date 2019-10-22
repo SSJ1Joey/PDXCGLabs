ec68a489a4ff3640ab699d9a3d7e6531b202f869
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-21 21:17:33 -0700

    yeep

---
+++ atm.py
@@ -0,0 +1,32 @@
+
+class atm():
+    
+    def __init__(self, balance = 0, amount = 0):
+        self.balance = balance
+        self.amount = amount
+
+    def check_balance():
+        return f'Here is your current balance: {self.balance}'
+
+    def deposit(self, amount):
+        x = int(input('How much would you like to deposit?: '))
+
+
+       
+       
+    def check_withdrawal(self, amount): 
+        return self.balance >= withdrawal
+        
+
+    def withdraw(self, amount): 
+         int(input('How much would you like to withdrawal?: '))
+
+
+
+    def print_transactions():
+        pass
+
+
+
+
+    
