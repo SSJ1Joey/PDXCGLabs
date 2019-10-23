9a86cac22520fffd7258e69f7cb67c83428a9674
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-22 19:48:47 -0700

    On the way

--- atm.py
+++ atm.py
@@ -1,8 +1,9 @@
 
 class atm:
     
-    def __init__(self, balance = 0):
+    def __init__(self, balance = 0, history = []):
         self.balance = balance
+        self.history = history
        
 
     
@@ -16,7 +17,7 @@ class atm:
         
         
     def check_withdrawal(self): 
-        return self.balance >= withdrawal
+        return self.balance >= self.withdraw
         
 
     def withdraw(self, amount):
@@ -24,11 +25,46 @@ class atm:
         return self.balance
          
 
-    def print_transactions():
-        pass
+    def print_transactions(self):
+        self.history.append(self.withdraw)
+        self.history.append(self.deposit)
+        print(self.history)
+
+        
 
 
 
-my_acc = atm()
-my_acc.deposit(15)
-print(my_acc.check_balance())
+my_account = atm()
+#my_account.deposit(15)
+#print(my_account.check_balance())
+
+
+while True:
+
+    x = int(input('\n 1.Deposit \n 2.Withdraw \n 3.Check Balance \n 4.History \n 5.Exit \n Please select a number for service: '))
+    if x == 1:
+       my_account.deposit(int(input('How much would you like to deposit?: ')))
+       print(f'Here is your balance: {my_account.check_balance()}')
+    
+    elif x == 2:
+        my_account.withdraw(int(input('How much would you like to withdrawal?: ')))
+        #my_account.check_withdrawal()
+        print(f'Here is your balance: {my_account.check_balance()}')
+
+    elif x == 3:
+         print(f'Here is your balance: {my_account.check_balance()}')
+
+    elif x == 4:
+        print(my_account.print_transactions())
+
+    else:
+        print('Thank you come again!')
+        break
+
+
+        
+
+    
+
+
+
