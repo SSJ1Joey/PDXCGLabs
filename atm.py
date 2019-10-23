c5672a8d02dbe339c384e41ab24a585ac08d7144
Joseph Rogers <joseph.rogers89@icloud.com>
2019-10-22 20:34:49 -0700

    :tada:

--- atm.py
+++ atm.py
@@ -13,21 +13,23 @@ class atm:
     
     def deposit(self, amount):
         self.balance += amount
+        self.history.append(f'You deposited ${amount}')
         return self.balance
         
         
-    def check_withdrawal(self): 
-        return self.balance >= self.withdraw
+    def check_withdrawal(self, amount): 
+        if self.balance < amount:
+            print('Sorry not enough funds...')
+            return False
         
 
     def withdraw(self, amount):
         self.balance -= amount
+        self.history.append(f'You withdrew ${amount}')
         return self.balance
          
 
     def print_transactions(self):
-        self.history.append(self.withdraw)
-        self.history.append(self.deposit)
         print(self.history)
 
         
@@ -44,18 +46,21 @@ while True:
     x = int(input('\n 1.Deposit \n 2.Withdraw \n 3.Check Balance \n 4.History \n 5.Exit \n Please select a number for service: '))
     if x == 1:
        my_account.deposit(int(input('How much would you like to deposit?: ')))
-       print(f'Here is your balance: {my_account.check_balance()}')
+       print(f'Here is your balance: ${my_account.check_balance()}')
     
     elif x == 2:
-        my_account.withdraw(int(input('How much would you like to withdrawal?: ')))
-        #my_account.check_withdrawal()
-        print(f'Here is your balance: {my_account.check_balance()}')
+        amount = int(input('How much would you like to withdrawal?: '))
+        if my_account.check_withdrawal(amount) == False:
+            continue
+        else:
+            my_account.withdraw(amount)
+        print(f'Here is your balance: ${my_account.check_balance()}')
 
     elif x == 3:
-         print(f'Here is your balance: {my_account.check_balance()}')
+         print(f'Here is your balance: ${my_account.check_balance()}')
 
     elif x == 4:
-        print(my_account.print_transactions())
+        my_account.print_transactions()
 
     else:
         print('Thank you come again!')
