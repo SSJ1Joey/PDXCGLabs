69b953ad45c73156362a0a8ebdea0aa355f03b46
Joseph Rogers <joseph.rogers89@icloud.com>
2019-11-01 10:53:34 -0700

    :bug:

--- blackjack.py
+++ blackjack.py
@@ -1,14 +1,11 @@
- #cards = {'A':1, 'K':10, 'Q':10, 'J':10, '10':10, '9':9, '8':8, '7':7, '6':6, '5':5, '4':4, '3':3, '2':2}
-
+ 
 def blackjack():
     
     cards = []
-    
+
     print('Choose 3 cards: A,K,Q,J,10,9,8,7,6,5,4,3,2\n')
 
-    
     x = input('What is your first card?: ')
-    
     if x == 'A':
         cards.append('1')
     elif x == 'K':
@@ -21,7 +18,6 @@ def blackjack():
         cards.append(x)
 
     y = input('What is your second card?: ')
-    
     if y == 'A':
         cards.append('1')
     elif y == 'K':
@@ -34,7 +30,6 @@ def blackjack():
         cards.append(y)
 
     z = input('What is your third card?: ')
-    
     if z == 'A':
         cards.append('1')
     elif z == 'K':
@@ -46,8 +41,10 @@ def blackjack():
     else:
         cards.append(z)
 
-    for i in cards:
-        cards = int(cards)
+    for i in range(len(cards)):
+        cards[i] = int(cards[i])
+
+    print(cards)
 
 
     if sum(cards) == 21:
@@ -56,7 +53,7 @@ def blackjack():
     elif sum(cards) < 17:
         print('Hit')
 
-    elif sum(cards) >= 17:
+    elif sum(cards) >= 17 and sum(cards) < 21:
         print('Stay')
 
     else:
