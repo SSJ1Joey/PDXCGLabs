2954a6c4bbe263b7d1814f2bf22253ddfd4c680c
Joseph Rogers <joseph.rogers89@icloud.com>
2019-11-19 19:46:36 -0800

    bull :poop:

--- pick6.js
+++ pick6.js
@@ -2,6 +2,7 @@ const readline = require('readline-sync');
 
 function ticket() {
     let user_ticket = []
+    for (let i = 0; i < 5; ++i)
     user_ticket.push(Math.floor(((Math.random() * 100) + 1)))
     console.log(user_ticket);
 }
